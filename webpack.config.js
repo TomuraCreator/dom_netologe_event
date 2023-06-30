ff51cdd7d567abda065e390c5d9fb08d5f71a793
Tryd0g0lik <work80@mail.ru>
2023-06-30 13:19:35 +0700

    repaet clone from is dom_netologe's project

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,85 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
+process.traceDeprecation = true;
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const miniCssExtractPlugin = require("mini-css-extract-plugin");
 const TsconfigPathPlugin = require("tsconfig-paths-webpack-plugin");
-// const loader = require("mini-css-extract-plugin/types/loader");
+const ImageminWebpWebpackPlugin = require("imagemin-webp-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin"); // https://webpack.js.org/plugins/copy-webpack-plugin/
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+//Node:  HtmlWebpackPlugin() - расскоментировать чтоб запустить webpack.dev.js
+//Node:  HtmlWebpackPlugin() - закоментировать чтоб запустить webpack.prod.js
 
-const isProduction = process.env.NODE_ENV == "production";
+require("webpack-dev-server");
 
 module.exports = {
+	mode: 'none',
 	target: 'web',
-  entry: "./src/index.ts",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-  },
+	entry: "./src/index.js",
 
-  plugins: [
-    new HtmlWebpackPlugin({
-			template: "./index.html",
-			filename: "./index.html",
-			minify: {
-				collapseWhitespace: false,
-			}
-    }),
+	output: {
+		path: path.resolve(__dirname, "dist"),
+	},
 
-		new TsconfigPathPlugin({
-			configFile: "./tsconfig.json"
-		})
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-  ],
-  module: {
-    rules: [
+
+	module: {
+		rules: [
 			{
-				exclude: ["/node_modules/"],
-				test: /\.(ts|tsx)$/i,
+				test: /\.ts$/i,
 				exclude: /node_modules/,
-				use: {
-					loader: ["ts-loader", "babel-loader"],
-				},
-				options: {
-					transpileOnly: true,
-				},
+				use: "ts-loader",
 
 			},
 			{
-				test: /\.html/,
+				test: /\.js$/i,
+				exclude: /node_modules/,
+				use: [
+					{ loader: "babel-loader" },
+				],
+			},
+			{
+				test: /\.html$/,
 				use: [
 					{
-						loader: "html-loader"
+						loader: "html-loader",
 					},
 				]
 			},
+			{
+				test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+				type: "asset/ressource",
+			},
 
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
-      },
+			// Add your rules for custom modules here
+			// Learn more about loaders from https://webpack.js.org/loaders/
+		],
+	},
 
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
-    ],
-  },
-  resolve: {
+	plugins: [
+
+		new CopyPlugin({
+			patterns: [
+				{
+					from: './src/pic',
+					to: './pic'
+				}
+			]
+		}),
+
+		new TsconfigPathPlugin({
+			configFile: "./tsconfig.json"
+		}),
+		// Add your plugins here
+		// Learn more about plugins from https://webpack.js.org/configuration/plugins/
+
+		// new HtmlWebpackPlugin({
+		// 	template: "./src/index.html",
+		// 	filename: "./index.html",
+		// 	minify: {
+		// 		collapseWhitespace: false,
+		// 	}
+		// }),
+	],
+
+	resolve: {
 		extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
 
 	},
--- webpack.dev.js
+++ webpack.dev.js
@@ -3,20 +3,36 @@ const webpack = require("webpack");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.config");
 const ESLintPlugin = require("eslint-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// HtmlWebpackPlugin() - закомментирован
 
 module.exports = merge(common, {
 	mode: "development",
+	devtool: 'source-map',//'cheap-module-source-map',
 
 	devServer: {
+		static: {
+			directory: path.join(__dirname, './dist'),
+		},
 		compress: true,
 		historyApiFallback: true,
-		open: true,
-		host: "localhost",
+		// open: true,
+		// host: "localhost",
+		port: 8080,
 	},
-	plugin: [
+	plugins: [
+		// new webpack.LoaderOptionsPlugin(),
 		new webpack.HotModuleReplacementPlugin(),
 		new ESLintPlugin({
 			files: path.resolve(__dirname, "./src/js"),
-		})
+		}),
+		// new HtmlWebpackPlugin({
+		// 	template: "./src/index.html",
+		// 	filename: "./index.html",
+		// 	minify: {
+		// 		collapseWhitespace: false,
+		// 	}
+		// }),
 	],
 });
+// cross-env TS_NODE_PROJECT=\"tsconfig-for-webpack-config.json\"
